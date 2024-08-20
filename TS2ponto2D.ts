3916a514405f9eb65ef7dbb1cef0b89d79b2a1b0
Gustav-hdoliveira <162641711+Gustav-hdoliveira@users.noreply.github.com>
2024-08-19 21:43:07 -0300

    Update TS2ponto2D.ts

--- TS2ponto2D.ts
+++ TS2ponto2D.ts
@@ -1,3 +1,4 @@
+let reader = require("readline-sync")
 class Ponto2D {
     constructor(){
 
@@ -44,17 +45,17 @@ class Ponto2DCordenadas extends Ponto2D {
         let diferençaX = this.x - outroPonto.x
         let diferençaY = this.y - outroPonto.y
         if (diferençaX <= 0) {
-            outroPonto.x - this.x
             console.log(`O x atual é menor que o do outro ponto`)
+        } else if (diferençaX == this.x) {
+            console.log('Os Valores são os mesmos')
         } else {
-            this.x - outroPonto.x
             console.log(`O x atual é maior que o do outro ponto`)
         }
         if (diferençaY <= 0) {
-            outroPonto.y - this.y
             console.log("O y atual é menor que o do outro ponto")
+        } else if (diferençaY == this.y) {
+            console.log('Os Valores são os mesmos')
         } else {
-            this.y - outroPonto.y
             console.log("O y atual é maior que o do outro ponto")
         }
     }
@@ -81,5 +82,63 @@ class PT2Dponto extends Ponto2D{
 }
 
 let PT2DCoordenadas = new Ponto2DCordenadas(12, 22)
-let PT2Dpontocp = new PT2Dponto(12, 23)
-PT2DCoordenadas.set(20, 33)
+let PT2Dpontocp = new PT2Dponto(PT2DCoordenadas)
+
+function exibirmenus() {
+    console.log("==============SISTEMA DE PONTO 2D==============")
+    console.log("1. settar.")
+    console.log("2. get.")
+    console.log("3. Mover coordenada")
+    console.log("4. Mover para string.")
+    console.log("5. Verificar se o valor é o mesmo ao anterior.")
+    console.log('6. Verificar a diferença entre os valores.')
+    console.log('7. Clonar')
+    console.log("8. SAIR.")
+}
+rodaralternativa();
+function processEscolha(opcao) {
+    switch (opcao) {
+        case '1':
+            let xUP = reader.question('Qual o valor do x? ')
+            let yUP = reader.question('Qual o valor do y? ')
+            PT2DCoordenadas.set(xUP, yUP)
+            break;
+        case '2':
+            PT2DCoordenadas.get()
+            break;
+        case '3':
+            let coord = reader.question('Deseja alterar o x ou o y?')
+            let nuum = reader.question("Qual o numero novo")
+            PT2DCoordenadas.movPonto(coord, nuum)
+            break;
+        case '4':
+            console.log(PT2DCoordenadas.toString())
+            break;
+        case '5':
+            console.log(PT2DCoordenadas.equals(PT2DCoordenadas))
+            break;
+        case '6':
+            PT2DCoordenadas.distancia(PT2DCoordenadas)
+            break;
+        case '7':
+            console.log(PT2DCoordenadas.clone())
+            break;
+        case '8':
+            console.log("Encerrando o programa...");
+            return true;
+    
+        default:
+            console.log("opção selecionada incorretamente")
+            break;
+    }
+    return false;
+}
+function rodaralternativa(): void {
+    let encerrar = false;
+
+    while (!encerrar) {
+        exibirmenus();
+        const opcao = reader.question("Digite o número da opção desejada: ");
+        encerrar = processEscolha(opcao);
+    }
+}
